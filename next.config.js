3868b905cb785dd87f4d955bf89e1f8702816377
Febri Milansyah <febrimilan00@gmail.com>
2024-01-09 15:51:59 +0700

    Update PwA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require('next-pwa')({
   dest: "public",
   register: true,
   skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
 })
 
 const nextConfig = withPWA({
